6f7001813f0408ec9c56a11ee856da42d36b35a4
sd.lee <sd.lee@ninetap.com>
2021-02-09 16:23:04 +0900

    [이상동] 텍스트 포맷 상수 추가

--- Scripts/Runtime/Define/KCDefine+Abs.cs
+++ Scripts/Runtime/Define/KCDefine+Abs.cs
@@ -277,6 +277,7 @@ public static partial class KCDefine {
 
 	// 형식 {
 	public const string B_TEXT_FMT_PRICE = "{0} {1}";
+	public const string B_TEXT_FMT_CURRENCY = "{0:N0}";
 	public const string B_TEXT_FMT_HH_MM_SS = "{0:00}:{1:00}:{2:00}";
 
 	public const string B_TEXT_FMT_1_DIGITS = "{0:0}";
