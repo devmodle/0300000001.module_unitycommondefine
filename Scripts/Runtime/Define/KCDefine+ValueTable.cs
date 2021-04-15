030aeb122f0b74c1cf2a2a9571fd4931080d87f8
sd.lee <sd.lee@ninetap.com>
2021-04-15 11:38:50 +0900

    [이상동] 불필요한 상수 제거

--- Scripts/Runtime/Define/KCDefine+ValueTable.cs
+++ Scripts/Runtime/Define/KCDefine+ValueTable.cs
@@ -30,6 +30,6 @@ public static partial class KCDefine {
 	// 경고 팝업 간격 }
 
 	// 게임 정보 저장소
-	public const string VT_KEY_DEF_DELAY_ADS = "DEF_DELAY_FULLSCREEN_ADS";
+	public const string VT_KEY_DEF_DELAY_ADS = "DEF_DELAY_ADS";
 	#endregion			// 기본
 }
