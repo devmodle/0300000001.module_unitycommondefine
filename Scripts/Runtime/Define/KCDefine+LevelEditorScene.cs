df326779567db12451cbb66d83605a7101221a2d
dante <are2341@nate.com>
2021-07-19 15:24:46 +0900

    [이상동] 상수 클래스 수정g

--- Scripts/Runtime/Define/KCDefine+LevelEditorScene.cs
+++ Scripts/Runtime/Define/KCDefine+LevelEditorScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 #if UNITY_EDITOR || UNITY_STANDALONE
 //! 레벨 에디터 씬 상수
-public static partial class KDefine {
+public static partial class KCDefine {
 	#region 기본
 	// 이름 {
 	public const string LES_OBJ_N_PORTRAIT_UIS = "PortraitUIs";
