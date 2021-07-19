b7aa029032ad4ee3d53879d711b63e38db3a53f7
dante <are2341@nate.com>
2021-07-19 12:38:45 +0900

    [이상동] 서브 모듈 갱신

---
+++ Scripts/Runtime/Define/KCDefine+LevelEditorScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+//! 레벨 에디터 씬 상수
+public static partial class KDefine {
+	#region 기본
+	// 이름
+	public const string LES_OBJ_N_LEFT_EDITOR_UIS = "LeftEditorUIs";
+	public const string LES_OBJ_N_MID_EDITOR_UIS = "MidEditorUIs";
+	public const string LES_OBJ_N_RIGHT_EDITOR_UIS = "RightEditorUIs";
+	#endregion			// 기본
+}
+#endif			// #if UNITY_EDITOR || UNITY_STANDALONE
