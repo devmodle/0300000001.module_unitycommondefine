022e18bfdbad60b3665a42353cc84584f57adde0
dante <are2341@nate.com>
2021-07-26 16:09:17 +0900

    [이상동] 유니티 모듈 버전 갱신 (1.6.1 => 1.7.0)

--- Scripts/Editor/Define/KCEditorDefine+Abs.cs
+++ Scripts/Editor/Define/KCEditorDefine+Abs.cs
@@ -28,7 +28,7 @@ public static partial class KCEditorDefine {
 	public const string B_SORTING_OI_FMT = "[{0}:{1}]";
 
 	// 버전
-	public const string B_VER_UNITY_MODULE = "1.6.1";
+	public const string B_VER_UNITY_MODULE = "1.7.0";
 
 	// 커맨드 라인
 	public const string B_CMD_LINE_PARAMS_FMT_SHELL = "-c \"{0}\"";
