99875a38ae9715dc000f5bc6f090476bfbf72704
dante <are2341@nate.com>
2021-12-25 03:57:24 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Editor/Define/KCEditorDefine+Abs.cs
+++ Scripts/Editor/Define/KCEditorDefine+Abs.cs
@@ -72,7 +72,7 @@ public static partial class KCEditorDefine {
 	public const string B_CMD_LINE_PARAMS_FMT_SHELL = "-c \"{0}\"";
 	public const string B_CMD_LINE_PARAMS_FMT_CMD_PROMPT = "/c \"{0}\"";
 
-	public const string B_BUILD_CMD_EXPORT_PATH = "export PATH=\"$PATH:/usr/local/bin\"";
+	public const string B_BUILD_CMD_EXPORT_PATH = "export PATH=\"${PATH}:/usr/local/bin\"";
 	// 커맨드 라인 }
 
 	// 이름 {
@@ -201,7 +201,7 @@ public static partial class KCEditorDefine {
 	public const string B_KEY_JENKINS_BUILD_FILE_EXTENSION = "BuildFileExtension";
 
 	public const string B_BUILD_PARAMS_TOKEN_JENKINS = " ";
-	public const string B_PIPELINE_GROUP_NAME_FMT_JENKINS = "job/02000001.Common/job/{0}/job/01.Pipelines/job";
+	public const string B_PIPELINE_GROUP_NAME_FMT_JENKINS = "job/00000001.Common/job/{0}/job/01.Pipelines/job";
 
 	public const string B_BUILD_DATA_FMT_JENKINS = "--data {0}={1}";
 	public const string B_BUILD_CMD_FMT_JENKINS = "curl -X POST {0} --user {1}:{2} --data token={3}";
