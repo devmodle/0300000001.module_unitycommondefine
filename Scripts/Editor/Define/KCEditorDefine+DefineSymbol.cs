5a97a2d396970b45f0ef387364760bc9cc116cd5
sd.lee <sd.lee@ninetap.com>
2022-03-15 11:20:47 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Editor/Define/KCEditorDefine+DefineSymbol.cs
+++ Scripts/Editor/Define/KCEditorDefine+DefineSymbol.cs
@@ -19,6 +19,7 @@ public static partial class KCEditorDefine {
 
 	// 버전
 	public const string DS_DEFINE_S_MODULE_VER_2_0_0_OR_NEWER = "MODULE_VER_2_0_0_OR_NEWER";
+	public const string DS_DEFINE_S_MODULE_VER_2_1_0_OR_NEWER = "MODULE_VER_2_1_0_OR_NEWER";
 
 	// 플러그인
 	public const string DS_DEFINE_S_APPLE_LOGIN_ENABLE = "APPLE_LOGIN_ENABLE";
