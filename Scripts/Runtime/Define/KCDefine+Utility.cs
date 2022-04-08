505cc276fdb6a6455b6992b3329d6364e35ca874
sd.lee <sd.lee@ninetap.com>
2022-04-09 05:16:38 +0900

    [이상동] 상수 스크립트 갱신

--- Scripts/Runtime/Define/KCDefine+Utility.cs
+++ Scripts/Runtime/Define/KCDefine+Utility.cs
@@ -551,6 +551,7 @@ public static partial class KCDefine {
 	public const string U_KEY_SERVICES_M_SIGNATURE = "signature";
 
 	public const string U_KEY_SERVICES_M_INIT_CALLBACK = "ServicesMInitCallback";
+	public const string U_KEY_SERVICES_M_LOAD_GOOGLE_SHEET_CALLBACK = "ServicesMLoadGoogleSheetCallback";
 	// 서비스 관리자 }
 
 	// 유니티 메세지 전송자 {
@@ -1215,11 +1216,11 @@ public static partial class KCDefine {
 	public const string U_FIELD_N_CLEAR_DEPTH = "m_ClearDepth";
 #endif			// #if UNIVERSAL_RENDERING_PIPELINE_MODULE_ENABLE
 
-#if UNITY_STANDALONE && GOOGLE_SHEET_MODULE_ENABLE
+#if UNITY_STANDALONE && GOOGLE_SHEET_MODULE_ENABLE && (DEBUG || DEVELOPMENT_BUILD)
 	// 이름
 	public const string U_CELL_N_GOOGLE_SHEET_SRC = "A1";
 	public const string U_CELL_N_GOOGLE_SHEET_DEST = "CZ10000";
-#endif			// #if UNITY_STANDALONE && GOOGLE_SHEET_MODULE_ENABLE
+#endif			// #if UNITY_STANDALONE && GOOGLE_SHEET_MODULE_ENABLE && (DEBUG || DEVELOPMENT_BUILD)
 	#endregion			// 조건부 상수
 
 	#region 조건부 런타임 상수
