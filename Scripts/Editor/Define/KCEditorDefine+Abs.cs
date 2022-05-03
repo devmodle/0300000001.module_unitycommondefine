cf8b3438ff3aaac743ef4c9578958a04ac34c677
sd.lee <sd.lee@ninetap.com>
2022-05-03 14:39:10 +0900

    [이상동] 모듈 버전 갱신 (2.3.21 => 2.3.22)

--- Scripts/Editor/Define/KCEditorDefine+Abs.cs
+++ Scripts/Editor/Define/KCEditorDefine+Abs.cs
@@ -46,7 +46,7 @@ public static partial class KCEditorDefine {
 	public const string B_SCENE_N_DIGITS_PATTERN = @"[\d.]";
 
 	// 버전
-	public const string B_VER_UNITY_MODULE = "2.3.21";
+	public const string B_VER_UNITY_MODULE = "2.3.22";
 
 	// 메뉴 {
 	public const string B_MENU_TOOLS_BASE = "Tools/Utility/";
