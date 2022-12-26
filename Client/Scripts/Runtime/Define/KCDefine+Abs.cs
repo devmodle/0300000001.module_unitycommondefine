e167f5c6d064b3c3f9440c20d50b64ffee5ab4a3
dante <lkstudio.dante@gmail.com>
2022-12-26 14:15:08 +0900

    [이상동] 스크립트 템플릿 갱신

--- Client/Scripts/Runtime/Define/KCDefine+Abs.cs
+++ Client/Scripts/Runtime/Define/KCDefine+Abs.cs
@@ -223,6 +223,9 @@ public static partial class KCDefine {
 	public const string B_PLATFORM_N_STANDALONE_WNDS_STEAM = "StandaloneWndsSteam";
 
 	public const string B_DIR_N_SCENES = "Scenes";
+	public const string B_DIR_N_EDITOR = "Editor";
+	public const string B_DIR_N_RUNTIME = "Runtime";
+	public const string B_DIR_N_SCRIPTS = "Scripts";
 	public const string B_DIR_N_EXTERNAL_DATAS = "ExternalDatas";
 	public const string B_EDITOR_SCENE_N_PATTERN_01 = "EditorMenu";
 	public const string B_EDITOR_SCENE_N_PATTERN_02 = "EditorScene";
