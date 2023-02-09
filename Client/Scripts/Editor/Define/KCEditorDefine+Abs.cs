d11eceed624d3aec612f1016128cf8a8402e4799
sd.lee <sd.lee@ninetap.com>
2023-02-09 13:38:32 +0900

    [이상동] 모듈 버전 갱신 (3.0.2 => 3.0.3)

--- Client/Scripts/Editor/Define/KCEditorDefine+Abs.cs
+++ Client/Scripts/Editor/Define/KCEditorDefine+Abs.cs
@@ -46,7 +46,7 @@ public static partial class KCEditorDefine {
 	public const string B_SORTING_OI_FMT = "[{0}:{1}]";
 
 	// 버전
-	public const string B_VER_UNITY_MODULE = "3.0.2";
+	public const string B_VER_UNITY_MODULE = "3.0.3";
 
 	// 메뉴 {
 	public const string B_MENU_TOOLS_BASE = "Tools/Utility/";
