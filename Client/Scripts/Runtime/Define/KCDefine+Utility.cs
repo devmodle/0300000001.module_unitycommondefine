4740ad05d90464b2333ecac1a2bb76f617f91b45
sd.lee <sd.lee@ninetap.com>
2023-04-10 11:51:40 +0900

    [이상동] 서브 모듈 갱신

--- Client/Scripts/Runtime/Define/KCDefine+Utility.cs
+++ Client/Scripts/Runtime/Define/KCDefine+Utility.cs
@@ -1370,6 +1370,7 @@ public static partial class KCDefine {
 
 #if PURCHASE_MODULE_ENABLE
 	// 기타
+	public const string U_ENVIRONMENT_N_DEV = "development";
 	public const string U_ENVIRONMENT_N_PRODUCTION = "production";
 	public const string U_PAYLOAD_PURCHASE_M_PURCHASE = "PurchaseMPurchase";
 
