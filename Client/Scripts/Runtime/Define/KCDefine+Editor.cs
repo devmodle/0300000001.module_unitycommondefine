8202c801a1519aa955ef971f6c26dd61e224eb6c
sd.lee <sd.lee@ninetap.com>
2023-04-17 11:30:02 +0900

    [이상동] 서브 모듈 갱신

--- Client/Scripts/Runtime/Define/KCDefine+Editor.cs
+++ Client/Scripts/Runtime/Define/KCDefine+Editor.cs
@@ -9,7 +9,7 @@ using UnityEngine.Events;
 public static partial class KCDefine {
 	#region 기본
 	// 비율
-	public const float ES_SCALE_SCREEN_HEIGHT = 1.25f;
+	public const float ES_SCALE_SCREEN_HEIGHT = 2.0f;
 
 	// 이름
 	public const string ES_OBJ_N_EDITOR_INPUT_POPUP = "EDITOR_INPUT_POPUP";
