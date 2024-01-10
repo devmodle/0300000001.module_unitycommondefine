1f23b68c4e7d21c606f1c46861d4038379f797fa
sd.lee <sd.lee@ninetap.com>
2024-01-10 16:13:46 +0900

    [이상동] 서브 모듈 갱신

---
+++ Client/Scripts/Editor/Define/KCEditorDefine+Text.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+/** 에디터 텍스트 상수 */
+public static partial class KCEditorDefine {
+	#region 상수
+	// 텍스트 {
+	public const string B_TEXT_APPLY = "적용";
+	public const string B_TEXT_SEARCH = "검색";
+	public const string B_TEXT_REPLACE = "변경";
+
+	public const string B_TEXT_FONT_REPLACE = "=====> 폰트 변경 <=====";
+	public const string B_TEXT_OBJ_NAME_REPLACE = "=====> 객체 이름 변경 <=====";
+	// 텍스트 }
+	#endregion // 상수
+}
+#endif // #if UNITY_EDITOR
