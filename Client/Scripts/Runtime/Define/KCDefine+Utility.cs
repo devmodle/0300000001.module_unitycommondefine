0e700e198942333c8e55bb32372f51c0567ff7d4
sd.lee <sd.lee@ninetap.com>
2024-01-26 15:37:52 +0900

    [이상동] 서브 모듈 갱신

--- Client/Scripts/Runtime/Define/KCDefine+Utility.cs
+++ Client/Scripts/Runtime/Define/KCDefine+Utility.cs
@@ -1024,10 +1024,10 @@ public static partial class KCDefine {
 	public static readonly string U_IMG_P_SPLASH = $"{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Splash";
 	public static readonly string U_IMG_P_INDICATOR = $"{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Indicator";
 
-	public static readonly string U_TEX_P_ARROW = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01__IGNORE_TC__/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Arrow";
-	public static readonly string U_TEX_P_WHITE = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01__IGNORE_TC__/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}White";
-	public static readonly string U_TEX_P_SPLASH = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01__IGNORE_TC__/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Splash";
-	public static readonly string U_TEX_P_INDICATOR = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01__IGNORE_TC__/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Indicator";
+	public static readonly string U_TEX_P_ARROW = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01{KCDefine.B_NAME_PATTERN_FILTER_SETUP_TEX_COMPRESS}/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Arrow";
+	public static readonly string U_TEX_P_WHITE = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01{KCDefine.B_NAME_PATTERN_FILTER_SETUP_TEX_COMPRESS}/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}White";
+	public static readonly string U_TEX_P_SPLASH = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01{KCDefine.B_NAME_PATTERN_FILTER_SETUP_TEX_COMPRESS}/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Splash";
+	public static readonly string U_TEX_P_INDICATOR = $"{KCDefine.B_DIR_P_TEXTURES}U_SpriteAtlas_01{KCDefine.B_NAME_PATTERN_FILTER_SETUP_TEX_COMPRESS}/{KCDefine.B_PREFIX_U_SPRITE_ATLAS_01}Indicator";
 
 #if UNITY_EDITOR
 	public static readonly string U_RUNTIME_TABLE_P_G_LEVEL_INFO = $"{KCDefine.B_ABS_DIR_P_EXTERNAL_DATAS}{KCDefine.U_TABLE_P_G_LEVEL_INFO}.json";
