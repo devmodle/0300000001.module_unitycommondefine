a90fe7eaac29f88f45afc49e04c88e4dfc9eaecb
sd.lee <sd.lee@ninetap.com>
2024-03-13 13:54:20 +0900

    [이상동] 서브 모듈 갱신

---
+++ Client/Scripts/Runtime/Define/KCDefine+TypeInterface.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+#region 인터페이스
+/** 풀링 인터페이스 */
+public partial interface IPoolable
+{
+	/** 풀링 여부를 변경한다 */
+	public void SetIsPooling(bool a_bIsPooling);
+}
+
+/** 갱신 인터페이스 */
+public partial interface IUpdatable
+{
+	/** 상태를 갱신한다 */
+	public void OnUpdate(float a_fDeltaTime);
+
+	/** 상태를 갱신한다 */
+	public void OnLateUpdate(float a_fDeltaTime);
+
+	/** 상태를 갱신한다 */
+	public void OnFixedUpdate(float a_fDeltaTime);
+
+	/** 상태를 갱신한다 */
+	public void OnCustomUpdate(float a_fDeltaTime);
+}
+#endregion // 인터페이스
